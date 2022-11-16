bc8cf516d4bc0138af8d36703ea02ccd0add4ef9
guilherme.schroder <guilhermeschroder@outlook.com>
2022-11-16 13:44:33 -0300

    ok

--- atividade04/atividade7.php
+++ atividade04/atividade7.php
@@ -2,7 +2,6 @@
 
 $cidades = array ('sc' => array('Rio do sul','Blumenau','joinville'),
                   'rs' => array('Porto Alegre','Santa Rosa','Caxias do Sul'),
-                  'pr' => array('Curitiba','Pato Branco','Londrina'))
+                  'pr' => array('Curitiba','Pato Branco','Londrina'));
 
-echo $cidades["sc"][1];
-
+print_r($cidades);
---
+++ atividade04/atividade8.php
@@ -0,0 +1,6 @@
+<?php
+
+$times = array("joinville" => array('2','5','4'),
+                "chapecoense" => array('1','9','6'),
+                "figuerense" => array('3','3','0'),
+                "avai" => array('4','1','-2'));
