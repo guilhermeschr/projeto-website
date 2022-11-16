2650a49bcd0e7af8630e7c0f42f200bbbe835a79
guilherme.schroder <guilhermeschroder@outlook.com>
2022-11-16 13:51:17 -0300

    ok

--- atividade04/atividade8.php
+++ atividade04/atividade8.php
@@ -4,3 +4,5 @@ $times = array("joinville" => array('2','5','4'),
                 "chapecoense" => array('1','9','6'),
                 "figuerense" => array('3','3','0'),
                 "avai" => array('4','1','-2'));
+
+print_r($times);
